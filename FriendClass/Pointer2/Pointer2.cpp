786be7a659d1dfb480514947786ef9c757e7953e
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-08 16:07:07 +0700

    int main

--- FriendClass/Pointer2/Pointer2.cpp
+++ FriendClass/Pointer2/Pointer2.cpp
@@ -12,6 +12,12 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	mahasiswa* mhs = new mahasiswa{ 1 };
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+	system("pause");
+	return 0;
+
 }
 
