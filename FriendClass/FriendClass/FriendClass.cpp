52a21d5ba8882557bc9b7772ec4d6269a85debcd
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-08 15:26:45 +0700

    Class orang

--- FriendClass/FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass/FriendClass.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+using namespace std;
+
+class siswa;
+
+class orang {
+private:
+    string nama;
+public :
+    void SetNama(string pNama);
+    friend class siswa;
+};
 
 int main()
 {
