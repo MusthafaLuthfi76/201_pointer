50fd84ecca0352f62b1b6d8003b1b8238bce5155
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-08 15:55:08 +0700

    class pelajar

--- FriendClass/FriendClass3/FriendClass3.cpp
+++ FriendClass/FriendClass3/FriendClass3.cpp
@@ -8,6 +8,15 @@ public :
     void ShowNilaiPelajar(pelajar&);
 };
 
+class pelajar {
+private:
+    int nilai;
+public :
+    pelajar() {
+        nilai = 100;
+    }
+    friend void manusia::ShowNilaiPelajar(pelajar& x);
+};
 int main()
 {
     std::cout << "Hello World!\n";
